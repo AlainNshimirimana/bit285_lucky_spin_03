eb126a0b37e62fb7812a709fc2c02315f393662d
Alain Nshimirimana <alain.nshimirimana@gmail.com>
2020-01-16 22:04:31 -0800

    Luckyspin 03 works but I don't know how to to get TextTransform to work

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -1,27 +1,31 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using LuckySpin.Models;
 
 namespace LuckySpin.Controllers
 {
-    public class SpinnerController : Controller
-    {
-        Random random = new Random();
 
-        //The Conroller to receive the Luck number from the Route
-        public IActionResult Index(int luck = 7) //Default value for luck is 7
-        {
-            int a = random.Next(1, 10);
-            int b = random.Next(1, 10);
-            int c = random.Next(1, 10);
+   public class SpinnerController : Controller
+   {
 
-            // Load up the ViewBag for use by the Spinner View "Index.cshtml"
-            ViewBag.ImgDisplay = (a == luck || b == luck || c == luck)?"block":"none";
-            ViewBag.A = a;
-            ViewBag.B = b;
-            ViewBag.C = c;
-            ViewBag.luck = luck;
+      Random random = new Random();
 
-            return View();
-        }
-    }
+      //The Conroller to receive the Luck number from the Route
+      public IActionResult Index(int luck = 7) //Default value for luck is 7
+      {
+         LuckySpin.Models.Spin spin = new Spin();
+         int a = random.Next(1, 10);
+         int b = random.Next(1, 10);
+         int c = random.Next(1, 10);
+
+         // Load up the ViewBag for use by the Spinner View "Index.cshtml"
+         spin.ImgDisplay = (a == luck || b == luck || c == luck) ? "block" : "none";
+         spin.A = a;
+         spin.B = b;
+         spin.C = c;
+         spin.luck = luck;
+
+         return View(spin);
+      }
+   }
 }
--- LuckySpin/Startup.cs
+++ LuckySpin/Startup.cs
@@ -16,7 +16,7 @@ namespace LuckySpin
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-        }
+      }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
